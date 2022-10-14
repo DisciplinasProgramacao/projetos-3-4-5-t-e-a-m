1fcbbf869b90e4142a9aedddee35dbca306f2740
André <afmendes@sga.pucminas.br>
2022-10-14 00:21:46 -0300

    Atualização Veiculos

--- codigo/src/Aplicacao/Veiculos.java
+++ codigo/src/Aplicacao/Veiculos.java
@@ -2,11 +2,11 @@ package Aplicacao;
 
 public class Veiculos {
 
-    private String placa;
-    private final int capacidadeTanque, percentualIpva, percentualSeguro,
-    private final float valorVenda, acrescimoSeguro, kmPorLitro;
+    private int placa;
+    private int capacidadeTanque, percentualIpva, percentualSeguro,
+    private float valorVenda, acrescimoSeguro, kmPorLitro;
 
-    public Veiculo (int placa, float valorVenda, int percentualIpva, int percentualSeguro, float acrescimoSeguro, float kmPorLitro){
+    public Veiculos(int placa, float valorVenda, int percentualIpva, int percentualSeguro, float acrescimoSeguro, float kmPorLitro) {
         this.placa = placa;
         this.valorVenda = valorVenda;
         this.percentualIpva = percentualIpva;
@@ -15,20 +15,20 @@ public class Veiculos {
         this.kmPorLitro = kmPorLitro;
     }
 
-    public float valorIpva(float valorVenda, int percentualIpva){
-        return (valorVenda * percentualIpva/100); 
+    public float valorIpva(float valorVenda, int percentualIpva) {
+        return (valorVenda * percentualIpva / 100);
     }
 
-    public float valorSeguro (float valorVenda, int percentualSeguro, float acrescimoSeguro){
-        return (valorVenda * percentualSeguro/100 + acrescimoSeguro);
+    public float valorSeguro(float valorVenda, int percentualSeguro, float acrescimoSeguro) {
+        return (valorVenda * percentualSeguro / 100 + acrescimoSeguro);
     }
 
-    public float autonomia (float kmPorLitro, int capacidadeTanque){
+    public float autonomia(float kmPorLitro, int capacidadeTanque) {
         return (kmPorLitro * capacidadeTanque);
     }
 
-    public outrosCustos(){
-        
-    }
+    // public outrosCustos(){
+
+    // }
 
 }
