1f430622b2cfb354e892d2fe549e30f6c7b68fbe
André <afmendes@sga.pucminas.br>
2022-10-21 11:52:50 -0300

    Atualizar

--- codigo/src/Aplicacao/Carro.java
+++ codigo/src/Aplicacao/Carro.java
@@ -1,6 +1,8 @@
 package Aplicacao;
 
 
+
+
 public class Carro extends Veiculo{
     private static final int CAPACIDADE_TANQUE = 50, PERCENTUAL_IPVA = 4, PERCENTUAL_SEGURO = 5, KM_ALINHAMENTO = 10000;
     private static final double ACRESCIMO_SEGURO = 300.00, VALOR_DO_ALINHAMENTO = 120.00, KM_POR_LITRO = 10;
