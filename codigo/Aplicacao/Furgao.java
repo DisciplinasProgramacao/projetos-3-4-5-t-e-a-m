909699acf3cf42b3b9869958562feff419ae6216
André <afmendes@sga.pucminas.br>
2022-11-22 03:26:59 -0300

    Atualização Veiculos

--- codigo/Aplicacao/Furgao.java
+++ codigo/Aplicacao/Furgao.java
@@ -3,6 +3,7 @@ public class Furgao extends Veiculo {
 	// ATRIBUTOS
 	private static final int PERCENTUAL_IPVA = 3;
 	private static final int PERCENTUAL_SEGURO = 3;
+	private static final int ACRESCIMO_SEGURO = 0;
 	private static final int KM_ALINHAMENTO = 10000;
 	private static final double VALOR_POR_ALINHAMENTO = 120.00;
 	private static final int KM_VISTORIA = 10000;
@@ -22,7 +23,7 @@ public class Furgao extends Veiculo {
 	private static Combustivel[] combustiveis = {Combustivel.GASOLINA};
 
 	public Furgao(String placa, double valorVenda, int kmAtual) {
-		super(placa, valorVenda, PERCENTUAL_IPVA, PERCENTUAL_SEGURO, kmAtual, CAPACIDADE_TANQUE, 
+		super(placa, valorVenda, PERCENTUAL_IPVA, PERCENTUAL_SEGURO, ACRESCIMO_SEGURO, kmAtual, CAPACIDADE_TANQUE, 
 		quantCombustivelAtual, custoCombustivel, custoVariavel, combustiveis);
 		this.kmAlinhamento = KM_ALINHAMENTO;
 		this.valorAlinhamento = VALOR_POR_ALINHAMENTO;
