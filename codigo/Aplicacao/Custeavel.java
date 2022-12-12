a55150815109540d4344d127cb26e056c2be82e0
André <afmendes@sga.pucminas.br>
2022-12-12 01:41:44 -0300

    Documentação

--- codigo/Aplicacao/Custeavel.java
+++ codigo/Aplicacao/Custeavel.java
@@ -1,11 +1,27 @@
 
 public interface Custeavel {
 
+    /**
+     * 
+     * @return Soma de todos os custos do veículo
+     */
     public double custoTotal();
 
+    /**
+     * 
+     * @return Custo do veículo com combustível.
+     */
     public double custoCombustivel();
 
+    /**
+     * Custo do veículo com alinhamento, manutenção e vistoria.
+     */
     public void custoVariavel();
 
+   
+    /**
+     * Custo do veículo com IPVA e seguro.
+     *  
+     */
     public double custoFixo();
 }
